f21ffc574e1a60fd86154f5141c3a577461e0585
Ben Nortier <bjnortier@gmail.com>
2014-03-19 18:40:28 +0000

    add/remove name in calendar

--- public/event.js
+++ public/event.js
@@ -4540,6 +4540,8 @@ var NewParticipantView = Backbone.View.extend({
 
 });
 
+var counter = 0;
+
 var ExistingParticipantView = Backbone.View.extend({
 
   initialize: function(options) {
@@ -4554,8 +4556,18 @@ var ExistingParticipantView = Backbone.View.extend({
   },
 
   render: function() {
+    var radioId = 'rd_' + (counter++);
     this.$el.html(
-      '<input type="radio" name="participants/><span class="name">' + this.name + '</span>');
+      '<input type="radio" name="participants" id="' + radioId + '" value="' + this.name + '"/><label for="' + radioId + '">' + this.name + '</label>');
+  },
+
+  events: {
+    'change input': 'change',
+  },
+
+  change: function() {
+    var name = $('input[name=participants]:checked', '#participants').val();
+    this.trigger('participantChanged', name);
   },
 
 });
@@ -4566,76 +4578,150 @@ var ParticipantsModel = Backbone.Model.extend({
   initialize: function(participants) {
 
     var newParticipantView = new NewParticipantView({model: this});
-    this.views = [
-      newParticipantView,
-    ];
-    this.views = this.views.concat(participants.map(function(name) {
-      new ExistingParticipantView({model: this, name: name});
-    }), this);
 
-    // Responsd to new participant added
     var that = this;
+    var onParticipantChange = function(name) {
+      that.trigger('participantSelected', name);
+    };
+    
+    this.participantViews = participants.map(function(name) {
+      var view = new ExistingParticipantView({model: this, name: name});
+      view.on('participantChanged', onParticipantChange);
+      return view;
+    });
+
+    // Respond to new participant added
+
     newParticipantView.on('newParticipant', function(name) {
-      $.post('/event/' + eventId + '/participant', {name: name}, function(result) {
-        console.log('>>>', result);
-        that.views.push(new ExistingParticipantView({model: this, name: name, selected: true}));
+
+      var existingView = that.participantViews.filter(function(view) {
+        return view.name === name;
       });
+
+      if (existingView.length) {
+        existingView[0].input.prop('checked',true);
+        onParticipantChange(name);
+      } else {
+        $.post('/event/' + eventId + '/participant', {name: name}, function() {
+          var view = new ExistingParticipantView({model: this, name: name, selected: true});
+          onParticipantChange(name);
+          view.on('participantChanged', onParticipantChange);
+          that.participantViews.push(view);
+        });
+      }
+
     });
   },
 
 });
 
+var MonthView = Backbone.View.extend({
+
+  initialize: function(options) {
+    this.chosenDays = {};
+    this.view = options;
+    this.render();
+    $('#months').append(this.$el);
+  },
+
+  render: function() {
+    var source   =
+      '{{name}} \
+       <table class="month" data-month="{{month}}"> \
+       <th>Su</th><th>Mo</th><th>Tu</th><th>We</th><th>Th</th><th>Fr</th><th>Sa</th> \
+       {{#each monthDays}} \
+         <tr class="week"> \
+         {{#each this}} \
+           {{#this}}<td class="day" data-day="{{.}}"><div class="label">{{.}}</div><div class="chosen"></div></td>{{/this}} \
+           {{^this}}<td></td>{{/this}} \
+         {{/each}} \
+         </tr> \
+       {{/each}} \
+       </table>';
+    var template = Handlebars.compile(source);
+    this.$el.html(template(this.view));
+  },
+  
+  events: {
+    'click .day': 'click',
+  },
+
+  click: function(event) {
+    var elem = $(event.currentTarget);
+    var day = elem.data('day');
+    var month = elem.parents('.month').data('month');
+    var participant = this.model.activeParticipant;
+    if (participant) {
+      var key = participant + '_' + month + '_' + day;
+      this.chosenDays[key] = !this.chosenDays[key];
+      if (this.chosenDays[key]) {
+        elem.find('.chosen').append($('<div class="participant" data-participant="' + participant + '">' + participant + '</div>'));
+      } else {
+        var elementsToRemove = elem.find('.chosen .participant').toArray().filter(function(maybeRemove) {
+          return '' + $(maybeRemove).data('participant') === participant;
+        });
+        elementsToRemove.forEach(function(elem) { $(elem).remove(); });
+      }
+    }
+  },
+
+});
+
+var DaysModel = Backbone.Model.extend({
+
+  initialize: function(attributes) {
+
+    var data = attributes.data;
+    var participantsModel = attributes.participantsModel;
+    var created = new Date(data.created);
+    var year = created.getFullYear();
+    var month = created.getMonth();
+    var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December'];
+
+    for (var i = 0; i < data.months; ++i) {
+      // Filter out zeroes
+      var monthDays = calendar.monthDays(year, month).reduce(function(acc, week) {
+        var filteredWeek = week.map(function(day) {
+          if (day !== 0) {
+            return day;
+          } else {
+            return undefined;
+          }
+        });
+        acc.push(filteredWeek);
+        return acc;
+      }, []);
+
+      new MonthView({
+        model: this,
+        month: month,
+        name: monthNames[month],
+        monthDays: monthDays
+      });
+    
+      ++month;
+      if (month === 12) {
+        month = 0;
+        ++year;
+      }
+    }
+
+    var that = this;
+    participantsModel.on('participantSelected', function(name) {
+      $('#months').removeClass('disabled');
+      that.activeParticipant = name;
+    });
+
+  },
+
+});
 
 $.get('/event/' + eventId, function(data) {
   $('#name').text(data.name);
   $('#description').text(data.description);
 
-  new ParticipantsModel(data.participants);
-
-  var created = new Date(data.created);
-  var year = created.getFullYear();
-  var month = created.getMonth();
-  var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December'];
-
-  var source   =
-      '{{name}} \
-       <table class="month"> \
-       <th>Su</th><th>Mo</th><th>Tu</th><th>We</th><th>Th</th><th>Fr</th><th>Sa</th> \
-       {{#each monthDays}} \
-         <tr class="week"> \
-         {{#each this}} \
-           <td>{{#this}}{{.}}{{/this}}</td> \
-         {{/each}} \
-         </tr> \
-       {{/each}} \
-       </table>';
-  var template = Handlebars.compile(source);
-
-  for (var i = 0; i < data.months; ++i) {
-    // Filter out zeroes
-    var monthDays = calendar.monthDays(year, month).reduce(function(acc, week) {
-      var filteredWeek = week.map(function(day) {
-        if (day !== 0) {
-          return day;
-        } else {
-          return undefined;
-        }
-      });
-      acc.push(filteredWeek);
-      return acc;
-    }, []);
-
-    $('#months').append(template({
-      name: monthNames[month],
-      monthDays: monthDays,
-    }));
-
-    ++month;
-    if (month === 12) {
-      month = 0;
-      ++year;
-    }
-  }
+  var participantsModel = new ParticipantsModel(data.participants);
+  new DaysModel({data: data, participantsModel: participantsModel});
 
 }).fail(function(err) {
   if (err.status === 404) {
